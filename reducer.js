7ae9d4324a59068471abdc3b18efb8483318e5a4
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-26 23:20:21 +0200

    add EDIT & THUMB ACTION in reducer.js

--- reducer.js
+++ reducer.js
@@ -1,5 +1,9 @@
 import {ADD_COMMENT} from './actions';
+import {EDIT_COMMENT} from './actions';
 import {REMOVE_COMMENT} from './actions';
+import {THUMB_UP_COMMENT} from './actions';
+import {THUMB_DOWN_COMMENT} from './actions';
+
 
 const initialState = {
     comments: [],
@@ -18,10 +22,22 @@ function reducer(state = initialState, action) {
                 }
                 , ...state]
             });
+        case EDIT_COMMENT:
+        	return Object.assign({}, state, {
+        		commments: state.comments.filter(comment => comment.id !== action.id)
+        	});
         case REMOVE_COMMENT:
             return Object.assign({}, state, {
                 comments: state.comments.filter(comment => comment.id !== action.id)
             });
+        case THUMB_UP_COMMENT;
+        	return Object.assign({}, state, {
+        		commments: state.comments.filter(comment => comment.id !== action.id)
+        	});
+        case THUMB_DOWN_COMMENT;
+        	return Object.assign({}, state, {
+        		commments: state.comments.filter(comment => comment.id !== action.id)
+        	});
         default:
             return state;
     }
