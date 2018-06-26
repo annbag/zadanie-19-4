902e9c8d1763dfdd8a4dc2b8df26944da724598b
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-26 23:31:55 +0200

    add content in reducer.js

--- reducer.js
+++ reducer.js
@@ -1,44 +1,13 @@
-import {ADD_COMMENT} from './actions';
-import {EDIT_COMMENT} from './actions';
-import {REMOVE_COMMENT} from './actions';
-import {THUMB_UP_COMMENT} from './actions';
-import {THUMB_DOWN_COMMENT} from './actions';
-
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
 const initialState = {
-    comments: [],
-    users: []
+	comments: [],
+	users: []
 };
 
-function reducer(state = initialState, action) {
-    switch(action.type) {
-        case ADD_COMMENT:
-            return Object.assign({}, state, {
-                comments: [
-                {
-                    id: action.id,
-                    text: action.text,
-                    votes: 0
-                }
-                , ...state]
-            });
-        case EDIT_COMMENT:
-        	return Object.assign({}, state, {
-        		commments: state.comments.filter(comment => comment.id !== action.id)
-        	});
-        case REMOVE_COMMENT:
-            return Object.assign({}, state, {
-                comments: state.comments.filter(comment => comment.id !== action.id)
-            });
-        case THUMB_UP_COMMENT;
-        	return Object.assign({}, state, {
-        		commments: state.comments.filter(comment => comment.id !== action.id)
-        	});
-        case THUMB_DOWN_COMMENT;
-        	return Object.assign({}, state, {
-        		commments: state.comments.filter(comment => comment.id !== action.id)
-        	});
-        default:
-            return state;
-    }
-}
+const app = combineReducers({
+    comments,
+    users
+});
